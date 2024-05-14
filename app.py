1848ffa6dcdde6610b5b09295c38ed0bde7f3adb
nehagslv <neha.gs@latentview.com>
2024-05-14 12:48:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,20 @@
-import streamlit as st
-import PyPDF4
-def extract_text(file):
-    if file.name.endswith('.pdf'):
-        pdf_reader = PyPDF4.PdfFileReader(file)
-        text = ''
-        for page_num in range(pdf_reader.numPages):
-            text += pdf_reader.getPage(page_num).extractText()
-        return text
-    elif file.name.endswith('.txt'):
-        return file.getvalue().decode('utf-8')
-    else:
-        return None
-def chatbot(input_text):
-    # Your chatbot logic here
-    response = "Hello! How can I assist you today?"
-    return response
-st.title("Document Parser")
-uploaded_file = st.file_uploader("Upload a PDF or text file", type=["pdf", "txt"])
-if uploaded_file is not None:
-    document_text = extract_text(uploaded_file)
-    if document_text:
-        st.write("### Uploaded Document Content:")
-    else:
-        st.write("Invalid file format. Please upload a PDF or text file.")
-        user_input = st.text_input("You:","")
-user_input = st.text_input("You:","")
-
-if user_input is not None:
-    # Check if user_input is not empty and not just whitespace
-    if user_input.strip() != "":
-        bot_response = chatbot(user_input)
-        st.text_area("Bot:", bot_response, height=100)
+import replicate 
+for event in replicate.stream(
+    "snowflake/snowflake-arctic-instruct",
+    input={
+        "top_k": 50,
+        "top_p": 0.9,
+        "prompt": "Generate a poem about the Python programming language.",
+        "temperature": 0.2,
+        "max_new_tokens": 512,
+        "min_new_tokens": 0,
+        "stop_sequences": "<|im_end|>",
+        "prompt_template": "<|im_start|>system\nYou're a helpful assistant<|im_end|>\n<|im_start|>user\n{prompt}<|im_end|>\n\n<|im_start|>assistant\n",
+        "presence_penalty": 1.15,
+        "frequency_penalty": 0.2
+    },
+):
+    print(str(event), end="")
 
 
 
