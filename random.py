8431e6b8aae174de732f252cc0500f1799d972e7
nehagslv <neha.gs@latentview.com>
2024-05-14 15:46:07 +0530

    Update random.py

--- random.py
+++ random.py
@@ -2,6 +2,9 @@ import streamlit as st
 import replicate
 import os
 from transformers import AutoTokenizer
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # App title
 st.set_page_config(page_title="Snowflake Arctic")
